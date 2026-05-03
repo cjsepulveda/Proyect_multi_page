67c7cf9e6a32522eeef07fb67cfbb03196f8dd65
cjsepulveda <cj.sepulveda.olivares@gmail.com>
2026-05-02 20:44:48 -0400

    update_87

--- src/pages/diadiagnostico.py
+++ src/pages/diadiagnostico.py
@@ -122,19 +122,30 @@ def update_charts(nivel,test,asig):
           mask01=df01[(df01['NIVEL'] == nivel) & (df01['UNIDAD ACADÉMICA'] == 'BÁSICA 1')]
           mask02=df01[(df01['NIVEL'] == nivel) & (df01['UNIDAD ACADÉMICA'] == 'BÁSICA 2')]
           mask03=df01[(df01['NIVEL'] == nivel) & (df01['UNIDAD ACADÉMICA'] == 'BÁSICA SF')]
+          mask04=df01[(df01['NIVEL'] == nivel) & (df01['UNIDAD ACADÉMICA'] == 'MEDIA LA')]
+          mask05=df01[(df01['NIVEL'] == nivel) & (df01['UNIDAD ACADÉMICA'] == 'MEDIA SF')]
 
           a ='Matemáticas'
           count_skill = [0,1,2,3]
           name_skill =['Números','Álgebra','Geometría','Datos y Azar']
           colors_skill=['#00308F','#03C03C','#ffbf00','#c91b00']
-
+          
+          
           graph_y_axes_SKILL_ua1=[mask01['num'],mask01['alg'],mask01['geo'],mask01['dat']]
           graph_y_axes_SKILL_ua2=[mask02['num'],mask02['alg'],mask02['geo'],mask02['dat']]
           graph_y_axes_SKILL_ua3=[mask03['num'],mask03['alg'],mask03['geo'],mask03['dat']]
+          graph_y_axes_SKILL_ua4=[mask04['num'],mask04['alg'],mask04['geo'],mask04['dat']]
+          graph_y_axes_SKILL_ua5=[mask05['num'],mask05['alg'],mask05['geo'],mask05['dat']]
+
+          count_ua_skill=[graph_y_axes_SKILL_ua1,graph_y_axes_SKILL_ua2,graph_y_axes_SKILL_ua3,graph_y_axes_SKILL_ua4,graph_y_axes_SKILL_ua5]
 
           graph_y_axes_average_ua1=mask01['prom_mat']
           graph_y_axes_average_ua2=mask02['prom_mat']
           graph_y_axes_average_ua3=mask03['prom_mat']
+          graph_y_axes_average_ua4=mask04['prom_mat']
+          graph_y_axes_average_ua5=mask05['prom_mat']
+
+          count_ua_average=[graph_y_axes_average_ua1,graph_y_axes_average_ua2,graph_y_axes_average_ua3,graph_y_axes_average_ua4,graph_y_axes_average_ua5]
 
           color_avr='#007fd2'
           
@@ -144,6 +155,8 @@ def update_charts(nivel,test,asig):
           mask01=df01[(df01['NIVEL'] == nivel) & (df01['UNIDAD ACADÉMICA'] == 'BÁSICA 1')]
           mask02=df01[(df01['NIVEL'] == nivel) & (df01['UNIDAD ACADÉMICA'] == 'BÁSICA 2')]
           mask03=df01[(df01['NIVEL'] == nivel) & (df01['UNIDAD ACADÉMICA'] == 'BÁSICA SF')]
+          mask04=df01[(df01['NIVEL'] == nivel) & (df01['UNIDAD ACADÉMICA'] == 'MEDIA LA')]
+          mask05=df01[(df01['NIVEL'] == nivel) & (df01['UNIDAD ACADÉMICA'] == 'MEDIA SF')]
 
           a ='Lenguaje'
           count_skill = [0,1,2]
@@ -153,20 +166,45 @@ def update_charts(nivel,test,asig):
           graph_y_axes_SKILL_ua1=[mask01['loc'],mask01['int'],mask01['ref']]
           graph_y_axes_SKILL_ua2=[mask02['loc'],mask02['int'],mask02['ref']]
           graph_y_axes_SKILL_ua3=[mask03['loc'],mask03['int'],mask03['ref']]
+          graph_y_axes_SKILL_ua4=[mask04['loc'],mask04['int'],mask04['ref']]
+          graph_y_axes_SKILL_ua5=[mask05['loc'],mask05['int'],mask05['ref']]
+
+          count_ua_skill=[graph_y_axes_SKILL_ua1,graph_y_axes_SKILL_ua2,graph_y_axes_SKILL_ua3,graph_y_axes_SKILL_ua4,graph_y_axes_SKILL_ua5]
 
           graph_y_axes_average_ua1=mask01['prom_len']
           graph_y_axes_average_ua2=mask02['prom_len']
           graph_y_axes_average_ua3=mask03['prom_len']
+          graph_y_axes_average_ua4=mask04['prom_len']
+          graph_y_axes_average_ua5=mask05['prom_len']
+
+          count_ua_average=[graph_y_axes_average_ua1,graph_y_axes_average_ua2,graph_y_axes_average_ua3,graph_y_axes_average_ua4,graph_y_axes_average_ua5]
 
           color_avr='#ffaf2b '
           print(df01)
     
     # Parámetros constantes para ambas asignaturas, petenecientes al descriptor NIVEL de LOGRO
     count_level=[0,1,2]
+
+    if nivel=='1MEDIO' or nivel=='2MEDIO':
+        count_ua =[3,4]
+        count_col=[0,1]
+        columnas = 2
+
+    else:
+        count_ua =[0,1,2]
+        count_col=[0,1,2]
+        columnas = 3
+
+    
+
     name_level=['Nivel I','Nivel II','Nivel III']
     graph_y_axes_LEVEL_ua1=[mask01['NIVEL I'], mask01['NIVEL II'], mask01['NIVEL III']]
     graph_y_axes_LEVEL_ua2=[mask02['NIVEL I'], mask02['NIVEL II'], mask02['NIVEL III']]
     graph_y_axes_LEVEL_ua3=[mask03['NIVEL I'], mask03['NIVEL II'], mask03['NIVEL III']]
+    graph_y_axes_LEVEL_ua4=[mask04['NIVEL I'], mask04['NIVEL II'], mask04['NIVEL III']]
+    graph_y_axes_LEVEL_ua5=[mask05['NIVEL I'], mask05['NIVEL II'], mask05['NIVEL III']]
+
+    count_ua_level=[graph_y_axes_LEVEL_ua1,graph_y_axes_LEVEL_ua2,graph_y_axes_LEVEL_ua3,graph_y_axes_LEVEL_ua4,graph_y_axes_LEVEL_ua5]
 
     colors_level=['#062c80','#0e6ac7','#4fb9fc']
     
@@ -175,6 +213,10 @@ def update_charts(nivel,test,asig):
     graph_x_axes_ua1 = [mask01['NIVEL'], mask01['ETAPA']]
     graph_x_axes_ua2 = [mask02['NIVEL'], mask01['ETAPA']]
     graph_x_axes_ua3 = [mask03['NIVEL'], mask01['ETAPA']]
+    graph_x_axes_ua4 = [mask04['NIVEL'], mask04['ETAPA']]
+    graph_x_axes_ua5 = [mask05['NIVEL'], mask05['ETAPA']]
+
+    x_axes_ua =[graph_x_axes_ua1,graph_x_axes_ua2,graph_x_axes_ua3,graph_x_axes_ua4,graph_x_axes_ua5]
 
     #print(mask01.iloc[:,1])
            
@@ -182,12 +224,13 @@ def update_charts(nivel,test,asig):
     
     if test == 'level_score': # Gráfica para NIVEL de LOGRO
             trace01 = make_subplots(
-                  rows=1, cols=3,
+                  rows=1, cols=columnas,
                   subplot_titles=("Básica 1", " Básica 2", "Básica SF"))
             trace01.update_annotations(font=dict(size=16, family="Arial", color="black"), font_weight="bold")
 
-            for x in count_level:
-                trace01.add_bar( x=graph_x_axes_ua1, y=graph_y_axes_LEVEL_ua1[x],
+            for ua, col in zip(count_ua, count_col):
+             for x in count_level:
+                    trace01.add_bar( x=x_axes_ua[ua], y=count_ua_level[ua][x],
                                 
                                 legendgroup="group1", showlegend=True,
                                 name=name_level[x],
@@ -203,43 +246,9 @@ def update_charts(nivel,test,asig):
                                                 font_family="Inter",
                                                 font_color="white"  # Sets the text color inside the box
                                                 ),
-                                row=1, col=1)
+                                row=1, col=col+1)
                 
-                trace01.add_bar( x=graph_x_axes_ua2, y=graph_y_axes_LEVEL_ua2[x], 
-                               
-                                legendgroup="group1", showlegend=False,
-                                name=name_level[x],
-                                texttemplate='%{y:.0%}',  # Format the labels as percentages with one decimal place
-                                textposition='inside',
-                                insidetextanchor='middle',  # Position of the labels
-                                textfont=dict(size=15, family="Arial", color="white", weight= 700),
-                                marker_color=colors_level[x],
-                                hovertemplate = new_hovertemplate,
-                                hoverlabel=dict(
-                                                #bgcolor="white",
-                                                #font_size=16,
-                                                font_family="Inter",
-                                                font_color="white"  , # Sets the text color inside the box
-                                                ),
-                                row=1, col=2)
-               
-                trace01.add_bar( x=graph_x_axes_ua3, y=graph_y_axes_LEVEL_ua3[x], 
-                               
-                                legendgroup="group1", showlegend=False,
-                                name=name_level[x],
-                                texttemplate='%{y:.0%}',  # Format the labels as percentages with one decimal place
-                                textposition='inside',
-                                insidetextanchor='middle',  # Position of the labels
-                                textfont=dict(size=15, family="Arial", color="white", weight= 700),
-                                marker_color=colors_level[x],
-                                hovertemplate = new_hovertemplate,
-                                hoverlabel=dict(
-                                                #bgcolor="white",
-                                                #font_size=16,
-                                                font_family="Inter",
-                                                font_color="white"  , # Sets the text color inside the box
-                                                ),
-                                row=1, col=3)
+                
             trace01.update_layout(barmode="relative", template='simple_white')
             b ='Niveles de Logro'
 
