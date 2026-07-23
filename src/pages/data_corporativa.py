74cfd40ab2517301806973aeb822692b629915ea
cjsepulveda <cj.sepulveda.olivares@gmail.com>
2026-07-23 10:53:11 -0400

    update_29_branch

--- src/pages/data_corporativa.py
+++ src/pages/data_corporativa.py
@@ -1,12 +1,12 @@
-import dash
 import pathlib
 from dash import html, dcc, callback, Input, Output, ALL, State, dash_table, register_page, no_update, exceptions
 import dash_bootstrap_components as dbc
 import plotly.graph_objects as go
+from plotly.subplots import make_subplots  
 import plotly.express as px
 import pandas as pd
 import numpy as np
-import copy
+
 
 register_page(
     __name__, 
@@ -17,12 +17,12 @@ register_page(
 
 PATH = pathlib.Path(__file__).parent
 DATA_PATH = PATH.joinpath("data").resolve()
-_df01_corp = _df02_cuota_mercado = _df03_cuota_mercado_basica = _df04_cuota_mercado_corp = None # cache en RAM
+_df01_corp = _df02_cuota_mercado = _df03_cuota_mercado_basica = _df04_cuota_mercado_corp = _df05_data_sae_corp = None # cache en RAM
 
 
 def load_data_corp():
     """Carga los datos de Excel solo una vez por proceso."""
-    global _df01_corp, _df02_cuota_mercado, _df03_cuota_mercado_basica, _df04_cuota_mercado_corp
+    global _df01_corp, _df02_cuota_mercado, _df03_cuota_mercado_basica, _df04_cuota_mercado_corp, _df05_data_sae_corp
     # Si ya se cargaron los datos, no hacemos nada.
     if _df01_corp is not None:
         return
@@ -32,6 +32,7 @@ def load_data_corp():
     _df02_cuota_mercado = pd.read_excel(workbook, sheet_name='data_mercado_media')
     _df03_cuota_mercado_basica = pd.read_excel(workbook, sheet_name='data_mercado_basica')
     _df04_cuota_mercado_corp = pd.read_excel(workbook, sheet_name='data_mercado_corp')
+    _df05_data_sae_corp = pd.read_excel(workbook, sheet_name='data_sae_corp')
 
 
 def obtener_datos_base():
@@ -49,6 +50,11 @@ def obtener_datos_mercado_basica():
 def obtener_datos_mercado_corp():
     load_data_corp()
     return _df04_cuota_mercado_corp
+
+def obtener_datos_sae_corp():
+    load_data_corp()
+    return _df05_data_sae_corp
+
 
 # Diccionario de Unidades Educativas
 unidades_edu = {
@@ -245,6 +251,57 @@ menu_lateral_mercado = html.Div([
     )
 ])
 
+menu_lateral_sae = html.Div([
+
+    dbc.Row(
+        dbc.Col(
+        # Tarjeta para SAE
+            dbc.Card([
+                dbc.CardHeader(html.Div([
+                                    html.H6(
+                                        [
+                                         html.I(className="fa-solid fa-database me-2"), 
+                                         'Data Sistema Admición Escolar SAE'
+                                         ],                                        
+                                        className="m-0 text-dark", style={"display": "inline"}),
+                                        html.Span(id="sae", className="text-white fw-bold", style={"display": "inline", "marginLeft": "5px"})
+                                     ], className="d-flex align-items-center"),
+                                    style={"backgroundColor": "#757575"}  # Color de fondo personalizado
+                                ),
+                dbc.CardBody(
+        # Lista despegable para SAE
+                        html.Div(
+                                children=[
+                                    html.H6(
+                                        [
+                                        html.I(className="fa-solid fa-school me-2"), 
+                                        'Unidades Educativas SAE'
+                                        ],
+                                        className="text-primary fw-bold mb-3"
+                                    ),
+                                    dcc.Dropdown(
+                                        id='unidades_sae', 
+                                        options=unidades_edu_options_dropdown,
+                                        value='CORPORACION',
+                                        clearable=False,
+                                        style={
+                                                'width': '100%',          # Ancho del dropdown
+                                                'backgroundColor': '#f0f0f0', # Color de fondo
+                                                'color': '#333333',      # Color del texto
+                                                'fontSize': '14px'       # Tamaño de la fuente
+                                            },
+                                        
+                                    ),
+                                ]),
+                                            )
+                            ], className="shadow-sm border-1", # fin tarjeta SAE
+                            
+                            ), # fin dbc SAE
+         ),className="mt-3" # fin columna para SAE
+    )
+])
+
+
 
 # Callback para cambiar opciones de niveles según unidad educativa elegida
 @callback (
@@ -260,7 +317,9 @@ def opciones_niveles(unidad_edu):
 
 layout = dbc.Container([
 
-     # Layaout General, 2 fila, cada fila con 2 columnas,
+     # Layaout General, 2 filas, cada fila con 2 columnas,
+     
+     # Primera Fila para menu matriculas y gráficos matrícula
      dbc.Row([
     
      # Columna para menu lateral
@@ -331,9 +390,9 @@ layout = dbc.Container([
             
             ],active_tab="tab-graf-1"), # Cierre de pestañas
           ], # cierre listas en la columna
-            width=8), # fin columna gráfico
+            width=8), # fin columna MATRICULA
 
-     ]), # Cierre, fila de layaout
+     ]), # Cierre, fila MATRICULA
      
      # Segunda fila para menu mercado y gráfico mercado
      dbc.Row([
@@ -359,7 +418,34 @@ layout = dbc.Container([
 
          ], 
             width=8), 
-     ])
+     ]),
+
+     # Tercera fila para menu SAE y gráfico SAE
+     dbc.Row([
+              dbc.Col( menu_lateral_sae, width=4), 
+              
+              dbc.Col([
+                 # Tarjeta para gráfico SAE
+                 dbc.Card([
+                     dbc.CardHeader(html.Div([
+                                             html.H6(" SAE: Sistema Admisión Estudiantes 2021-2025: ", className="m-0 text-dark", style={"display": "inline"}),
+                                             html.Span(id="titulo-grafico-sae", className="text-white fw-bold", style={"display": "inline", "marginLeft": "5px"})
+                                             ], className="d-flex align-items-center"),
+                                             style={"backgroundColor":"#309930"}  # Otro color de fondo
+                                     ),
+                     dbc.CardBody(
+                         dcc.Loading(
+                             id="corp-loading-grafico",
+                             type="circle",
+                             children=dcc.Graph(config={"displayModeBar": False}, id="grafico-sae"),
+                         )
+                     )
+                     ], className="shadow-sm mt-3"), # fin card
+     
+              ], 
+                 width=8), 
+          ]),
+
   ], fluid=True) # Fin Layaout, cierre dbc.Container
 
 # Callback para opciones gráficos CORPORACION
@@ -372,7 +458,7 @@ layout = dbc.Container([
     Output('titulo-grafico-captacion', 'children'),
     Output('niveles_educativos', 'disabled'),
     Input('unidades_educativas_corp', 'value'),
-    Input('niveles_educativos', 'value'),
+    Input('niveles_educativos', 'value')
     )
 def graficos_corporativos(unidad_educativa, nivel_educativo):
 
@@ -479,7 +565,7 @@ def graficos_corporativos(unidad_educativa, nivel_educativo):
 # Callback para opciones graficos MERCADO
 @callback(
         Output('grafico-corp-mercado', 'figure'),
-        Input('unidades_cuota_mercado','value'),
+        Input('unidades_cuota_mercado','value')
         )
 def mercado_graficos(unidad_mercado):
 
@@ -499,6 +585,32 @@ def mercado_graficos(unidad_mercado):
     grafico_mercado = generar_grafico_mercado(df_corp_filter_mercado, unidad_mercado)
     
     return grafico_mercado
+
+
+# Callback pata opciones grafico SAE
+@callback(
+        Output('grafico-sae', 'figure'),
+        Input('unidades_sae','value'),
+        )
+def sae_graficos(sae_unidad_educativa):
+
+    if sae_unidad_educativa =="CORPORACION":
+
+        df_corp_sae = obtener_datos_sae_corp()
+        df_corp_sae_drop = df_corp_sae.drop(["UNIDAD_ACADEMICA","% ADMITIDOS"], axis=1)
+        df_agrupado = df_corp_sae_drop.groupby("PERIODO").sum(numeric_only=True).reset_index()
+        
+        df_agrupado["% ADMITIDOS"]=df_agrupado["ADMITIDOS SAE"]/df_agrupado["POSTULANTES SAE"]
+      
+        grafico_corp_sae = generar_grafico_sae(df_agrupado)        
+    else:
+
+        df_corp_sae = obtener_datos_sae_corp()
+        df_corp_filter_sae = df_corp_sae.query("UNIDAD_ACADEMICA == @sae_unidad_educativa").copy()
+        
+        grafico_corp_sae = generar_grafico_sae(df_corp_filter_sae)
+        
+    return grafico_corp_sae
 
 # Funcion para crear graficos Matrícula, retencion y Captación
 def generar_graficos_corp(df_filtrado, data_x, data_y , marker_color, line_color, formato_num):
@@ -616,40 +728,39 @@ def generar_grafico_mercado(data_mercado_unidad, unidad_mercado_grafico):
         porcentaje_mercado = ((df_mercado[mercado]/df_mercado["TOTAL"])*100).round(1)
 
         graph_mercado.add_trace(
-        go.Bar(
-            x=df_mercado['PERIODO'], 
-            y=df_mercado[mercado], 
-            marker_color = color,
-            name=mercado,
-            text = porcentaje_mercado.astype(str) + "%",
-            textposition= "inside",
-            insidetextanchor= "middle",
-            textfont=dict(
-            size=16,                     
-            color="white",
-            weight = "bold"                                
-            ),
-            customdata=df_mercado[mercado],
-            hovertemplate=f"<b>{mercado}</b>"
-                           ": %{customdata:,}"
-                           "<extra></extra>"
-             )
-         )
+            go.Bar(
+                x=df_mercado['PERIODO'], 
+                y=df_mercado[mercado], 
+                marker_color = color,
+                name=mercado,
+                text = porcentaje_mercado.astype(str) + "%",
+                textposition= "inside",
+                insidetextanchor= "middle",
+                textfont=dict(
+                size=16,                     
+                color="white",
+                weight = "bold"                                
+                ),
+                customdata=df_mercado[mercado],
+                hovertemplate=f"<b>{mercado}</b>"
+                            ": %{customdata:,}"
+                            "<extra></extra>"
+                )
+            )
         
     if unidad_mercado_grafico in ['PARV BÁSICA 1','PARV BÁSICA 2','PARV BÁSICA SAN FELIPE']:
         graph_mercado.update_traces(name="PARV_PROVINCIA", selector=dict(name="BASICAS_PROV"))
     
     graph_mercado.update_layout(
-                                    hoverlabel_font=dict(family='Roboto mono', weight='bold', size=14, color='black'),
-                                    font_family='Roboto mono',
-                                    template="simple_white",
-                                    barmode='stack',
-                                    barnorm='percent',  # Escala automáticamente las barras al 100%
-                                    yaxis_ticksuffix='%',  # Añade el símbolo % al eje Y
-                                    # legend_title_text='Categorías',
-                                    margin=dict(l=40, r=30, t=10, b=10),
-                                    hovermode="x unified" 
-                                    )
+                        hoverlabel_font=dict(family='Roboto mono', weight='bold', size=14, color='black'),
+                        font_family='Roboto mono',
+                        template="simple_white",
+                        barmode='stack',
+                        barnorm='percent',
+                        yaxis_ticksuffix='%',
+                        margin=dict(l=40, r=30, t=10, b=10),
+                        hovermode="x unified" 
+                        )
         
     graph_mercado.update_xaxes(tickfont_weight='normal', 
                          tickfont_size=14, 
@@ -672,3 +783,84 @@ def generar_grafico_mercado(data_mercado_unidad, unidad_mercado_grafico):
 
 
     return graph_mercado
+
+# Función crear grafico SAE
+def generar_grafico_sae(df_sae_unidad_educativa):
+
+    df_sae= df_sae_unidad_educativa
+    x_df_sae = df_sae["PERIODO"]
+   
+
+    categorias_sae = ["POSTULANTES SAE", "ADMITIDOS SAE"]
+    colores_sae = ["#3067FD","#FFA21F"]
+
+    graph_sae = make_subplots ( specs = [[{ "secondary_y" :  True }]])
+
+    for sae_value, color in zip(categorias_sae, colores_sae):
+
+        graph_sae.add_trace(
+            go.Bar(
+                x=x_df_sae, 
+                y=df_sae[sae_value],
+                marker_color = color,
+                name=sae_value,
+                 ),
+                secondary_y=False,
+                )
+
+    graph_sae.add_trace(
+        go.Scatter(
+            x=x_df_sae,
+            y=df_sae["% ADMITIDOS"],
+            name="% Admitidos",
+            mode="lines+markers",
+            line=dict(color="#5F5F5F", width=3),
+            marker=dict(color = "#ffffff", size = 12, 
+                                line=dict(width = 2,
+                                color = "#BB0000")),
+            
+            ),
+            secondary_y=True,
+            )
+    
+    graph_sae.update_layout(
+                            hoverlabel_font=dict(family='Roboto mono', weight='bold', size=14, color='black'),
+                            font_family='Roboto mono',
+                            template="simple_white",
+                            margin=dict(l=40, r=30, t=10, b=10),
+                            hovermode="x unified",
+                            yaxis2=dict(
+                                title="Porcentaje de Admisión",
+                                tickformat=".0%",
+                                range=[0, 1],
+                                overlaying="y",
+                                side="right"
+                                ),
+                            legend=dict(
+                                orientation = "h",
+                                x=0.15,          # Posición horizontal (0 = izquierda, 1 = derecha)
+                                y=1.1,          # Posición vertical (0 = abajo, 1 = arriba)
+                                xanchor="left",
+                                yanchor="top"
+                            )
+                            )
+    
+    graph_sae.update_xaxes(tickfont_weight='normal', 
+                                 tickfont_size=14, 
+                                 showgrid=False,
+                                 ticks="", 
+                                 showline=False,
+                                 title_text="",
+                                 tickfont=dict(color='gray'),
+                                 )
+
+    graph_sae.update_yaxes(tickfont_weight='normal', 
+                                 showgrid=False, 
+                                 tickfont_size=14,
+                                 showline=True, 
+                                 linecolor="gray",
+                                 title_text="",
+                                 tickfont=dict(color='gray'),
+                                 )
+
+    return graph_sae
